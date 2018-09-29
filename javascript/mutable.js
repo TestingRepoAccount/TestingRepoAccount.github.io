484396646845fd30ff0acc7e563eda1afff0e87a
milesmf <mfalk@msae.edu>
2018-09-29 11:28:23 -0700

    pushing opacity changes

--- javascript/mutable.js
+++ javascript/mutable.js
@@ -2,19 +2,33 @@
 
 window.onload = function () {
 
+    //Store full url
     const url = window.location.href;
     
+    //Parse url and retrieve usefull info
     function parseURL (url) {
         const start = url.indexOf("?") + 1;
         const end = url.indexOf("#", start);
-        const data = url.substring(start, end);
-        return data;
+        let data = url.substring(start, end);
+        //Remove equal sign
+        data = data.replace('=', '/');
+        //Return completed/formated string
+        return `https://player.vimeo.com/${data}`
     }
 
+    //Append text to page title
     const title_h1 = document.querySelector('#video-info');
     title_h1.textContent = parseURL(url);
 
-    // console.log();
+    //Create new iframe element
+    const body = document.querySelector('body > section');
+    const video = document.createElement('iframe');
+
+    //Set src attribute
+    video.setAttribute('src', parseURL(url));
+
+    //Append video to page
+    body.appendChild(video);
 
     // console.log(parseURL(url));
 }
--- javascript/results.js
+++ javascript/results.js
@@ -8,17 +8,11 @@ window.onload = function () {
     //Handle click and redirect routes for video
     video.forEach((node) => {
         node.addEventListener('click', (element) => {
-            // let target = element.target;
-            // target = target.getAttribute('data-route');
-
             const route = 'mutable.html';
             
             let video = element.target
             video = video.getAttribute('data-video');
             video = video.split('vimeo.com/')[1];
-
-            // let password = element.target;
-            // password = password.getAttribute('data-pw');
 
             let data = `${route}?video=${video}`;
 
